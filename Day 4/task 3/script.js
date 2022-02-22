9cde3d7ed6eb063c0b6ebe87eaadc05102e6b749
Sujeet Rajak <79070041+MrAwsm99@users.noreply.github.com>
2022-02-22 16:42:52 +0530

    day 4

---
+++ Day 4/task 3/script.js
@@ -0,0 +1,10 @@
+function data(){
+    var state=document.getElementById("pstate").value;
+    var district=document.getElementById("pdistrict").value;
+ 
+
+    document.getElementById("rstate").value=district;
+    document.getElementById("rdistrict").value=state;
+
+   
+}
---
+++ Day 4/task 4/script.js
@@ -0,0 +1,7 @@
+function data(){
+    let name = document.getElementById("fname").value;
+    let city = document.getElementById("city").value;
+
+    document.getElementById("rename").innerHTML=name;
+    document.getElementById("recity").innerHTML=city;
+}
